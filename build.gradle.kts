65d09c2245fd21b7cd1f023d852dc573b28cb998
Oscar Chevalier <oscar@frozenmilk.dev>
2025-12-27 01:20:34 +1100

    chore: updated to match plugin changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,40 @@
 plugins {
-	id("dev.frozenmilk.android-library") version "10.1.1-0.1.3"
-	id("dev.frozenmilk.publish") version "0.0.4"
-	id("dev.frozenmilk.doc") version "0.0.4"
+    id("dev.frozenmilk.android-library") version "11.0.0-1.0.0"
+    id("dev.frozenmilk.publish") version "0.0.5"
+    id("dev.frozenmilk.doc") version "0.0.5"
 }
 
 // TODO: modify
 android.namespace = "com.example.library"
 
-// Most FTC libraries will want the following
 ftc {
-	kotlin // if you don't want to use kotlin, remove this
+    // adds support for kotlin
+    kotlin()
 
-	sdk {
-		RobotCore
-		FtcCommon {
-			configurationNames += "testImplementation"
-		}
-	}
+    sdk {
+        // adds the most common ftc library dependencies,
+        // you may need more
+        compileOnly(FtcCommon)
+        testImplementation(FtcCommon)
+
+        compileOnly(RobotCore)
+    }
 }
 
 publishing {
-	publications {
-		register<MavenPublication>("release") {
-			// TODO: modify
-			groupId = "com.example"
-			// TODO: modify
-			artifactId = "Library"
+    publications {
+        register<MavenPublication>("release") {
+            // TODO: modify
+            groupId = "com.example"
+            // TODO: modify
+            artifactId = "Library"
 
-			artifact(dairyDoc.dokkaHtmlJar)
-			artifact(dairyDoc.dokkaJavadocJar)
+            artifact(dairyDoc.dokkaHtmlJar)
+            artifact(dairyDoc.dokkaJavadocJar)
 
-			afterEvaluate {
-				from(components["release"])
-			}
-		}
-	}
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
